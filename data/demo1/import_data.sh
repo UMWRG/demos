8881038a4c24170ed4bfa7e5c72f357fc6279fcb
stephen <s.knox@ucl.ac.uk>
2015-03-13 17:26:43 +0000

    Demo 3 data 3 template updated with more types and data updated accordingly.

--- data/demo1/import_data.sh
+++ data/demo1/import_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ -n $1 ]; then
-python ~/git/HYDRA/HydraPlugins/CSVplugin/ImportCSV/ImportCSV.py -t network.csv -n nodes.csv -l links.csv -g groups.csv -k group_members.csv -x -m ../../templates/demo1/template.xml
+python ~/git/HYDRA/HydraPlugins/CSVplugin/ImportCSV/ImportCSV.py -t network.csv -n nodes.csv -l links.csv -g groups.csv -k group_members.csv -x -m ../../templates/demo1/template/template.xml
 else
-python ~/git/HYDRA/HydraPlugins/CSVplugin/ImportCSV/ImportCSV.py -i $1 -s "$2" -t network.csv -n nodes.csv -l links.csv -g groups.csv -k group_members.csv -x -m ../../templates/demo1/template.xml
+python ~/git/HYDRA/HydraPlugins/CSVplugin/ImportCSV/ImportCSV.py -i $1 -s "$2" -t network.csv -n nodes.csv -l links.csv -g groups.csv -k group_members.csv -x -m ../../templates/demo1/template/template.xml
 fi
