d2bb948c119888abb19b3d09bd948f160740c3fd
stephen <s.knox@ucl.ac.uk>
2015-03-26 14:48:28 +0000

    Put in an export call that uses start time, end time, time step.

--- gams_demo_suite/Demo2/export_data.sh
+++ gams_demo_suite/Demo2/export_data.sh
@@ -1,1 +1,2 @@
-python ~/git/hydra_plugins/GAMS/Export/GAMSExport.py -t $1 -s $2 -tx "2000-01-01, 2000-02-01, 2000-03-01, 2000-04-01, 2000-05-01, 2000-06-01" -o hydra_output.txt
+#python ~/git/hydra_plugins/GAMS/Export/GAMSExport.py -t $1 -s $2 -tx "2000-01-01, 2000-02-01, 2000-03-01, 2000-04-01, 2000-05-01, 2000-06-01" -o hydra_output.txt
+python ../../../hydra_plugins/GAMS/Export/GAMSExport.py -t $1 -s $2 -st "01/01/2000" -en "01/08/2000" -dt "1 mon" -o hydra_output.txt
