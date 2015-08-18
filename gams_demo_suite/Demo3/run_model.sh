595f1fd064eeaceacdd2ffbfcf5dbb50be1f52f9
stephen <s.knox@ucl.ac.uk>
2015-08-18 12:11:52 +0100

    Specify run model with time axis as well as start date & end date

--- gams_demo_suite/Demo3/run_model.sh
+++ gams_demo_suite/Demo3/run_model.sh
@@ -1,1 +1,2 @@
 python ../../../hydra_plugins/GAMS/Auto/GAMSAutoRun.py -t $1 -s $2 -st "01/01/2000" -en "01/06/2000" -dt "1 mon" -m Demo3.gms -o hydra_output.txt
+#python ../../../hydra_plugins/GAMS/Auto/GAMSAutoRun.py -t $1 -s $2 -tx 01/01/2000, 01/02/2000, 01/03/2000, 01/04/2000, 01/05/2000 ,01/06/2000 -m Demo3.gms -o hydra_output.txt
