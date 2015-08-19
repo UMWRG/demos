da6863bc97ad8b586a369e6db1243f70c8e777ff
stephen <s.knox@ucl.ac.uk>
2015-08-19 15:30:58 +0100

    D

--- gams_demo_suite/Demo3/run_model.sh
+++ gams_demo_suite/Demo3/run_model.sh
@@ -1,2 +1,2 @@
-python ../../../hydra_plugins/GAMS/Auto/GAMSAutoRun.py -t $1 -s $2 -st "01/01/2000" -en "01/06/2000" -dt "1 mon" -m Demo3.gms -o hydra_output.txt
-#python ../../../hydra_plugins/GAMS/Auto/GAMSAutoRun.py -t $1 -s $2 -tx 01/01/2000, 01/02/2000, 01/03/2000, 01/04/2000, 01/05/2000 ,01/06/2000 -m Demo3.gms -o hydra_output.txt
+#python ../../../hydra_plugins/GAMS/Auto/GAMSAutoRun.py -t $1 -s $2 -st "2000/01/01" -en "2000/06/01" -dt "1 mon" -m Demo3.gms -o hydra_output.txt
+python ../../../hydra_plugins/GAMS/Auto/GAMSAutoRun.py -t $1 -s $2 -tx 2000/01/01,  2000/02/01,  2000/03/01, 2000/04/01, 2000/05/01 ,2000/06/01 -m Demo3.gms -o hydra_output.txt
